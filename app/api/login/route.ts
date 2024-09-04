086807fcaa24ed33c9be005a4eef600b925f5662
kaixin20190930 <liukai19911010@gmail.com>
2024-09-04 16:35:55 +0800

    v0.5 add login and other pages

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,11 +1,6 @@
 import {NextRequest, NextResponse} from 'next/server';
-import {D1Database} from '@cloudflare/workers-types';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-
-declare global {
-    const DB: D1Database;
-}
 
 export async function POST(request: NextRequest) {
     const {email, password} = await request.json();
@@ -17,7 +12,7 @@ export async function POST(request: NextRequest) {
             return Response.json({message: 'User not found'}, {status: 404});
         }
 
-        const isValidPassword = await bcrypt.compare(password, user.password);
+        const isValidPassword = await bcrypt.compare(password, user.password as any);
 
         if (!isValidPassword) {
             return Response.json({message: 'Invalid password'}, {status: 401});
@@ -26,7 +21,7 @@ export async function POST(request: NextRequest) {
         const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET as string, {expiresIn: '1h'});
 
         const response = NextResponse.json({message: 'Login successful', token});
-        response.cookies.set('token' as any, token, {
+        response.cookies.set('token' as any, token as any, {
             httpOnly: true,
             secure: true,
             sameSite: 'strict',
