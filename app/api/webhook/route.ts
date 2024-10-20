54be61c24c005bba35a29784116d3177aff39a6d
kaixin20190930 <liukai19911010@gmail.com>
2024-10-20 13:53:55 +0800

    v1.2.1  change apiVersion: '2024-09-30.acacia',

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,7 +5,7 @@ import {Env} from '@/worker/types';
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2023-10-16',
+    apiVersion: '2024-09-30.acacia',
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
