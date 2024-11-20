b4e0b98f536ea3c0bc68f0459948887fc15c33e9
kaixin20190930 <liukai19911010@gmail.com>
2024-11-20 21:30:12 +0800

    v1.2.6  add multiple language

--- next.config.js
+++ next.config.js
@@ -12,6 +12,8 @@ const nextConfig = {
     },
     env: {
         JWT_SECRET: process.env.JWT_SECRET,
+        STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+        STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
     },
 };
 
