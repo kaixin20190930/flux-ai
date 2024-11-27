0f937cbc4c628810d8313522e2e3ede01c7e87a4
kaixin20190930 <liukai19911010@gmail.com>
2024-11-27 10:13:37 +0800

    v1.2.7 add google login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,8 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import type {Session, Token} from 'next-auth'
 
+export const runtime = 'edge';
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
