36f55da610563bbb0ce2423ee3efbfa145d504a5
kaixin20190930 <liukai19911010@gmail.com>
2024-11-27 14:40:58 +0800

    v1.2.7 add google login

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
-/** @types {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['replicate.delivery'],
     },
     experimental: {
-        webpackBuildWorker: true
+        webpackBuildWorker: true,
+        runtime: 'edge' // 添加这行
+    },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            crypto: require.resolve('crypto-browserify'),
+            stream: require.resolve('stream-browserify'),
+            http: require.resolve('stream-http'),
+            https: require.resolve('https-browserify'),
+            os: require.resolve('os-browserify/browser'),
+            url: require.resolve('url'),
+            assert: require.resolve('assert'),
+            buffer: require.resolve('buffer'),
+            querystring: require.resolve('querystring-es3'),
+        };
+        return config;
     },
     env: {
         JWT_SECRET: process.env.JWT_SECRET,
